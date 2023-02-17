d9a939f374ae89b2d552f486b4d21d0f53f8904f
DragiboO <dragibo.scotch@gmail.com>
2023-02-17 15:29:15 +0100

    :sparkles: chart + transaction

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let graph = document.querySelector('#chart').getContext('2d')
+
+new Chart(graph, {
+    type: 'line',
+    data: {
+        labels: ['5Nov','6Nov','7Nov','8Nov','9Nov','10Nov','11Nov','12Nov','13Nov','14Nov','15Nov'],
+        datasets: [
+            {
+                label: 'ETH',
+                data: [100, 120, 140, 130, 310, 400, 280, 210, 150, 130, 100, 110],
+                borderColor: 'rgba(25, 25, 25, 1)',
+                backgroundColor: 'transparent',
+            }
+        ]
+    }
+})
